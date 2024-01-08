015eda8e756d038db93edf196d019f26c3075665
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-08 23:50:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+
+    // Display form values using alert
+    var message = "First Name: " + firstName + "\n" +
+                  "Last Name: " + lastName + "\n" +
+                  "Phone Number: " + phone + "\n" +
+                  "Email: " + email;
+alert(message);
